da53b4b39b2d9cc67dd965409fc984e059e5b643
khusnitdin <khusnitdin@MacBook-Pro-khusnitdin.local>
2025-08-11 18:01:18 +0500

    create fonts and logo

--- app/components/hello.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-function Hello() {
-    console.log("client component")
-  return (
-    <div>Hello</div>
-  )
-}
-export default Hello
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <div>page</div>
-  )
+    <>
+      <h1 className="h1-bold">Welcoe to ts </h1>
+      <h1 className="h1-bold font-inter">Welcoe to ts Inter </h1>
+      <h1 className="h1-bold font-space-grotesk">Welcoe to ts S Grotesk </h1>
+    </>
+  );
 }
