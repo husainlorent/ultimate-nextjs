ed35a7946f98ef5e8a9a4a637b528f3881d5e86c
khusnitdin <khusnitdin@MacBook-Pro-khusnitdin.local>
2025-08-12 09:40:59 +0500

    create gitignore file

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ export default function Home() {
   return (
     <>
       <h1 className="h1-bold">Welcoe to ts </h1>
-      <h1 className="h1-bold font-inter">Welcoe to ts Inter </h1>
-      <h1 className="h1-bold font-space-grotesk">Welcoe to ts S Grotesk </h1>
     </>
   );
 }
